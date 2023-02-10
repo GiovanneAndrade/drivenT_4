658369726f2a808a075e74a48c4b86fc3a75b912
GiovanneAndrade <vanne.as@hotmail.com>
2023-02-10 14:21:41 -0300

    :test_tube:	test:  factories booking

---
+++ tests/factories/booking-factory.ts
@@ -0,0 +1,36 @@
+import dayjs from "dayjs";
+import faker from "@faker-js/faker";
+import { Event } from "@prisma/client";
+import { prisma } from "@/config";
+
+export async function createBooking(userId: number, roomId: number) {
+ return prisma.booking.create({
+    data: {
+      userId: userId,
+      roomId: roomId,
+    },
+  });
+}
+
+export async function findBooking(userId: number, roomId: number) {
+  return await prisma.booking.findFirst({
+    where: {
+      userId: userId,
+    },
+    select: {
+      id: true,
+      Room: true,
+    },
+  });
+ }
+ export async function findBookingRoomById(roomId: number) {
+  return await prisma.room.findMany({
+    where: {
+      id: roomId,
+    },
+    select:{
+      capacity:true,
+      Booking:true
+    }
+  });
+ }
