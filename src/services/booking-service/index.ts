6f92b8dd2878c6273700573a7f7bcbceb8ded804
GiovanneAndrade <vanne.as@hotmail.com>
2023-02-13 15:51:47 -0300

    :sparkles:	feat: booking server update booking

--- src/services/booking-service/index.ts
+++ src/services/booking-service/index.ts
@@ -1,5 +1,5 @@
 import bookingRepository from '@/repositories/booking-repository';
-import { notFoundError } from '@/errors';
+import { notFoundError, unauthorizedError } from '@/errors';
 
 async function findBooking(userId: number) {
   const result = await bookingRepository.findBooking(userId);
@@ -9,16 +9,35 @@ async function findBooking(userId: number) {
 
 async function createBooking(userId: number, roomId: number) {
   const consult = await bookingRepository.consultRoom(roomId);
-   
-  if (consult.length === 0 || consult[0].Booking.length === consult[0].capacity) throw notFoundError();
+
+  if (consult.length === 0) throw notFoundError();
+  if (consult[0].Booking.length === consult[0].capacity) throw unauthorizedError();
 
   const result = await bookingRepository.createBooking(userId, roomId);
   return result;
 }
 
+async function updateBooking(bookingId: number, userId: number, roomId: number) {
+  const consult = await bookingRepository.consultRoom(roomId);
+ 
+  const consultBookingUser = await bookingRepository.findBooking(userId);
+  if (!consultBookingUser) throw notFoundError();
+
+  if (consult.length === 0) throw notFoundError();
+  if (consult[0].Booking.length === consult[0].capacity) throw unauthorizedError();
+
+  const result = await bookingRepository.updateBooking(bookingId, userId, roomId);
+   
+  if (!result.count) throw notFoundError();
+  const consultBooking = await bookingRepository.ConsultBooking(roomId);
+    
+  return consultBooking;
+}
+
 const bookingService = {
   findBooking,
   createBooking,
+  updateBooking,
 };
 
 export default bookingService;
