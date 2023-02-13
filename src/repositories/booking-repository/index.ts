173facb5079a870279c1433657bef068410b5254
GiovanneAndrade <vanne.as@hotmail.com>
2023-02-13 15:52:27 -0300

    :sparkles:	feat: booking repository update booking

--- src/repositories/booking-repository/index.ts
+++ src/repositories/booking-repository/index.ts
@@ -11,9 +11,21 @@ async function findBooking(userId: number) {
     },
   });
 }
+async function ConsultBooking(roomId: number) {
+  return prisma.booking.findFirst({
+    where: {
+    roomId:roomId
+    },
+    select: {
+      roomId:true,
+      userId:true,
+      createdAt:true,
+      updatedAt:true
+    },
+  });
+}
 
 async function createBooking(userId: number, roomId: number) {
-
   return prisma.booking.create({
     data: {
       userId: userId,
@@ -21,24 +33,38 @@ async function createBooking(userId: number, roomId: number) {
     },
   });
 }
+
+async function updateBooking(bookingId: number, userId: number, roomId: number) {
+  return prisma.booking.updateMany({
+    where: {
+      id: bookingId,
+    },
+    data: {
+      userId: userId,
+      roomId: roomId,
+    },
+
+  });
+}
+
 async function consultRoom(roomId: number) {
   return prisma.room.findMany({
     where: {
       id: roomId,
     },
-    select:{
-      capacity:true,
-      Booking:true
-    }
+    select: {
+      capacity: true,
+      Booking: true,
+    },
   });
 }
-
 
 const bookingRepository = {
   findBooking,
   createBooking,
   consultRoom,
-
+  updateBooking,
+  ConsultBooking
 };
 
 export default bookingRepository;
